cffed46034fa2096a96ccdafdef98dfc598296ea
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-04-07 13:03:46 +0530

    Create Bubble_sortRECURSIVE.cpp

---
+++ Bubble_sortRECURSIVE.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubbleRec(vector<int> &arr,int n){
+    if (n == 1) return;
+    for (int j = 0; j <= n - 2; j++) {
+        if (arr[j] > arr[j + 1])swap(arr[j],arr[j+1]);
+    }
+    //Range reduced after recursion:
+    bubbleRec(arr, n - 1);
+}
+
+int main() {
+    vector<int> arr={3,2,1,1,2,3};
+    cout<<"Before Sorting : {";
+    for(auto it:arr)cout<<it<<" ";
+    cout<<"}\n";  
+    int n=arr.size();
+    bubbleRec(arr,n);
+    cout<<"After Sorting : {";
+    for(auto it:arr)cout<<it<<" ";
+    cout<<"}\n";
+    return 0;
+}
