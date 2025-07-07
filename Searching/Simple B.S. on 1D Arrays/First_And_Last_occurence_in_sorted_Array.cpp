1972156da5b5385c165ee5f8a3329e1341ad2a33
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-07-07 15:15:56 +0530

    Create First_And_Last_occurence_in_sorted_Array.cpp

---
+++ Searching/Simple B.S. on 1D Arrays/First_And_Last_occurence_in_sorted_Array.cpp
@@ -0,0 +1,42 @@
+//https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+class Solution {
+public:
+  int last(vector<int>& nums, int target){
+     int ans=-1;
+     int l=0,h=nums.size()-1;
+     while(l<=h){
+        int mid= l +((h-l)/2);
+
+        if(nums[mid]==target){
+            ans=mid;l=mid+1;
+        }
+        else if(nums[mid]<target)l=mid+1;
+        else h=mid-1;
+     }
+     return ans;
+   }
+
+   int first(vector<int>& nums, int target){
+     int ans=-1;
+     int l=0,h=nums.size()-1;
+     while(l<=h){
+        int mid= l +((h-l)/2);
+
+        if(nums[mid]==target){
+            ans=mid;h=mid-1;
+        }
+        else if(nums[mid]<target)l=mid+1;
+        else h=mid-1;
+     }
+     return ans;
+   }
+
+
+
+
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        return {first(nums,target),last(nums,target)};
+    }
+};
