30908cf71fe26de75cd548e0fffe1f28f8c09bf3
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-07-08 11:01:24 +0530

    Update Sorting.h

--- Sorting.h
+++ Sorting.h
@@ -1,52 +1,74 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
-    // Write C++ code here
-    cout << "Try programiz.pro\n";
-    
-    vector<int> arr={3,2,1,1,2,3};
-    cout<<"Before Sorting : {";
-    for(auto it:arr)cout<<it<<" ";
-    cout<<"}\n";
-    
-    int n=arr.size();
-    
-    // (SELECTION SORT)
-    // for(int i=0;i<n-1;i++){
-    //     int mini=i;
-        
-    //     for(int j=i+1;j<n;j++){
-    //         if(arr[j]<arr[mini])mini=j;
-    //     }
-        
-    //     if(mini!=i)swap(arr[mini],arr[i]);
-        
-    // }
-    // cout<<"After Sorting : {";
-    // for(auto it:arr)cout<<it<<" ";
-    // cout<<"}\n";
-    
-    // (BUBBLE SORT)
-    // for(int i=0;i<n-1;i++){
-    //     for(int j=0;j<n-i-1;j++){
-    //         if(arr[j]>arr[j+1])swap(arr[j],arr[j+1]);
-    //     }
-    // }
-    // cout<<"After Sorting : {";
-    // for(auto it:arr)cout<<it<<" ";
-    // cout<<"}\n";
-    
-    
-//     (Insertion Sort)
-//   for(int i=0;i<n;i++){
-//       for(int j=0;j<=i;j++){
-//           if(arr[j]>arr[j+1])swap(arr[j],arr[j+1]);
-//       }
-//   }
-//       cout<<"After Sorting : {";
-//     for(auto it:arr)cout<<it<<" ";
-//     cout<<"}\n";
 
+void selectionSort(vector<int> &arr,int n){
+    for(int i=0;i<n-1;i++){
+        int mini=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[mini])mini=j;
+        }
+        if(i!=mini)swap(arr[i],arr[mini]);
+    }
+}
+
+void bubbleSort(vector<int> &arr,int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1])swap(arr[j+1],arr[j]);
+        }
+       
+    }
+}
+
+void bubbleSortRec(vector<int> &arr,int n){
+   if(n==1)return;
+        for(int j=0;j<n-1;j++){
+            if(arr[j]>arr[j+1])swap(arr[j+1],arr[j]);
+        }
+       bubbleSortRec(arr,n-1);//sorts 1 element from end hence reducing search space by 1
+    
+}
+
+void insertionSort(vector<int> &arr,int n){
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+
+        /* Move elements of arr[0..i-1], that are
+           greater than key, to one position ahead
+           of their current position */
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+}}
+
+
+int main() {
+   vector<int> arr={7,4,1,8,5,2,9,6,3};
+   cout<<"Before Sorting : [ ";
+   for(auto it:arr)cout<<it<<" ";
+   cout<<"]\n";
+   
+   int n=arr.size();
+   // Sorting Algorithm
+   
+   //Selection sort
+   //selectionSort(arr,n);
+   
+   //Bubble sort
+   //bubbleSort(arr,n);
+   
+   //Bubble sort Recursive
+   //bubbleSortRec(arr,n);
+   
+   //Insertion Sort
+   //insertionSort(arr,n);
+   
+   cout<<"After Sorting : [ ";
+   for(auto it:arr)cout<<it<<" ";
+   cout<<"]\n";
 
     return 0;
 }
