28c835fc9264423a60beb44cc2ebee56f002b864
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-07-08 09:06:45 +0530

    Update Bubble_sortRECURSIVE.cpp

--- Bubble_sortRECURSIVE.cpp
+++ Bubble_sortRECURSIVE.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void bubbleRec(vector<int> &arr,int n){
     if (n == 1) return;
-    for (int j = 0; j <= n - 2; j++) {
+    for (int j = 0; j < n - 1; j++) {
         if (arr[j] > arr[j + 1])swap(arr[j],arr[j+1]);
     }
     //Range reduced after recursion:
